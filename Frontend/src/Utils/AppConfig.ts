47b4c541483f4a757f841de856aad2998fa98bcd
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-12-09 16:25:19 +0200

    edited appconfig

--- Frontend/src/Utils/AppConfig.ts
+++ Frontend/src/Utils/AppConfig.ts
@@ -1,24 +1,22 @@
 class AppConfig {
+    public readonly homeRoute: string = "/home/";
+    public readonly loginRoute: string = "/login/";
+    public readonly registerRoute: string = "/register/";
 
     public readonly productsRoute: string = "/products/"
     public readonly addProductRoute: string = this.productsRoute + "new/"
     public readonly editProductRoute: string = this.productsRoute + "edit/"
     public readonly editProductDetails: string = this.productsRoute + "details/"
 
-    public readonly CategoriesRoute: string = "/categories/"
-    public readonly CategoriesDetailsRoute: string = this.CategoriesRoute + "details/"
-
-    public readonly homeRoute: string = "/home/";
-    public readonly loginRoute: string = "/login/";
+    public readonly vacationsRoute: string = "/vacations/";
+    public readonly addVacationRoute: string = this.vacationsRoute + "new/"
+    public readonly editVacationRoute: string = this.vacationsRoute + "edit/"
+    public readonly editVacationDetails: string = this.vacationsRoute + "details/"
 
     public readonly productsUrl: string = 'http://localhost:4000/api/products/';
-    public readonly employeesUrl: string = `http://localhost:4000/api/employees/`;
+    public readonly vacationsUrl: string = 'http://localhost:4000/api/vacations/';
     public readonly registerUrl: string = 'http://localhost:4000/api/register/';
     public readonly loginUrl: string = 'http://localhost:4000/api/login/';
-    public readonly categoriesUrl: string = 'http://localhost:4000/api/categories/';
 }
-
-// Singleton
 const appConfig = new AppConfig();
-
 export default appConfig;
