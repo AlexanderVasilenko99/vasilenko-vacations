362a63af682c5a2ee1be1f6b3fdd096c8ffef480
Alexander Vasilenko <alexandervjr1@gmail.com>
2023-12-16 14:56:12 +0200

    filter is now a functioning accordion

--- Frontend/src/Components/VacationsArea/VacationsList/VacationsList.tsx
+++ Frontend/src/Components/VacationsArea/VacationsList/VacationsList.tsx
@@ -11,6 +11,7 @@ import UseTitle from "../../../Utils/UseTitle";
 function VacationsList(): JSX.Element {
     UseTitle("Vasilenko Vacations | Vacations");
     const [vacations, setVacations] = useState<VacationModel[]>([]);
+    const [accordionOpen, setAccordionOpen] = useState<boolean>(false);
     const navigate = useNavigate();
     useEffect(() => {
         const token = authStore.getState().token;
@@ -27,11 +28,12 @@ function VacationsList(): JSX.Element {
         <div className="VacationsList">
             <h1>Browse All</h1>
             <h2 className="general-info"><span>Or search a vacation:</span><span className="results">Showing {vacations.length} results</span></h2>
-            <div className="filter-container">
+            <div className={accordionOpen ? "filter-container accordion-open" : "filter-container accordion-close"}>
                 <div className="filter-headers">
-                    <h2 className="filter">Filter</h2>
+                    <h2 className="filter" onClick={() => setAccordionOpen(!accordionOpen)}>Filter</h2>
                     <h2 className="reset">Reset</h2>
                 </div>
+                <div className="filter-hidden-content">Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut exercitationem impedit molestias ipsum quasi corrupti possimus, deleniti, dolores, suscipit facilis tempora in aliquam veritatis quis voluptates provident illum laborum eos assumenda nesciunt cum sequi dolorem rem. Excepturi fugit dicta eveniet!</div>
             </div>
             <div className="vacations-container">
                 {vacations.map(v => <VacationCard
