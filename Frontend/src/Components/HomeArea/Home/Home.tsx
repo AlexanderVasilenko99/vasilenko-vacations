686c6bbdc396b998f31f05f9faff679c0154aacf
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-01-13 14:55:27 +0200

    removed scrollbar visibility

--- Frontend/src/Components/HomeArea/Home/Home.tsx
+++ Frontend/src/Components/HomeArea/Home/Home.tsx
@@ -8,7 +8,9 @@ function Home(): JSX.Element {
 
     return (
         <div className="Home">
-            <NavLink to={"https://www.linkedin.com/in/alexander-vasilenko-323a21299/"} target="_blank">
+            <NavLink to={"https://www.linkedin.com/in/alexander-vasilenko-323a21299/"}
+                target="_blank"
+                title="hire me pls">
                 <ReactPlayer url={require("../../../Assets/Videos/intro-vid.mp4")}
                     muted playing loop width="100%" height="100%" />
             </NavLink>
