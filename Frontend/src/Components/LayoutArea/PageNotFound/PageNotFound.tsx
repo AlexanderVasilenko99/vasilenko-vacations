46018c4ee8e4b59d991be301b2b27b5039ac52ac
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-01-17 15:51:58 +0200

    added 404 page

--- Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
+++ Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
@@ -1,12 +1,19 @@
 import "./PageNotFound.css";
 import UseTitle from "../../../Utils/UseTitle";
-
+import backgroundImage from "../../../Assets/Images/UtilityImages/404.png"
+import { NavLink } from "react-router-dom";
 function PageNotFound(): JSX.Element {
-    UseTitle("NorthWind | 404");
+    UseTitle("Vasilenko Vacations | 404");
     return (
         <div className="PageNotFound">
-            
-        </div>
+
+            <img src={backgroundImage} alt="" />
+            <div>
+                <p>Looks like you are looking for<br /> a page that doesn't exists!</p>
+                <p>Maybe you would like to try our <NavLink to="/vacations">vacations</NavLink> page!</p>
+            </div>
+
+        </div >
     );
 }
 
