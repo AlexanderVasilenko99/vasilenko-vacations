d9ecb5c9f044f2bb1a6c1821826569986d6921fd
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-01-18 19:08:47 +0200

    added scroll to top component

--- Frontend/src/Components/LayoutArea/Layout/Layout.tsx
+++ Frontend/src/Components/LayoutArea/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import FooterArea from "../../FooterArea/FooterArea";
 import NavbarArea from "../../NavbarArea/NavbarArea";
 import Routing from "../Routing/Routing";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
 import "./Layout.css";
 function Layout(): JSX.Element {
     return (
         <div className="Layout">
+            <ScrollToTop />
             <nav><NavbarArea /></nav>
             <main><Routing /></main>
             <footer><FooterArea /></footer>
