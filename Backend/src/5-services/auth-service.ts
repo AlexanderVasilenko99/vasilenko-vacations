5cd2002006d61bc5c519391ca70ddcf838c94fa9
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-01-22 09:36:40 +0200

    changed indentation on backend authservice

--- Backend/src/5-services/auth-service.ts
+++ Backend/src/5-services/auth-service.ts
@@ -1,10 +1,4 @@
-// class AuthService{
-//     public register(){
-
-//     }
-// }
-// const authService = new AuthService();
-// export default authService;
+import { randomUUID } from 'crypto';
 import { OkPacket } from 'mysql';
 import cyber from '../2-utils/cyber';
 import dal from '../2-utils/dal';
@@ -12,10 +6,16 @@ import CredentialsModel from '../3-models/credentials-model';
 import { EmailTaken, Unauthorized } from '../3-models/error-models';
 import RoleModel from '../3-models/role-model';
 import UserModel from '../3-models/user-model';
-import { randomUUID } from 'crypto';
+
 class AuthService {
     private async isEmailTaken(email: string): Promise<boolean> {
-        const sql = "SELECT userUUID,userFirstName,userLastName,userEmail FROM users WHERE userEmail = ?"
+        const sql = `SELECT
+            userUUID,
+            userFirstName,
+            userLastName,
+            userEmail
+            FROM users
+            WHERE userEmail = ?`
         const info: OkPacket = await dal.execute(sql, [email]);
 
         if (info[0]) return true;
@@ -26,15 +26,15 @@ class AuthService {
 
         user.addUserValidate();
 
-        if (await this.isEmailTaken(user.userEmail)) throw new EmailTaken("This email is taken!");
+        if (await this.isEmailTaken(user.userEmail))
+            throw new EmailTaken("Sorry but it seems like this email is already being used!");
 
-        user.userRoleId = RoleModel.User;
         user.userUUID = randomUUID();
-
+        user.userRoleId = RoleModel.User;
         user.userPassword = cyber.hashPassword(user.userPassword);
 
-        const sql = `INSERT INTO users VALUES(?,?,?,?,?,?,?)`;
 
+        const sql = `INSERT INTO users VALUES(?,?,?,?,?,?,?)`;
         const info: OkPacket = await dal.execute(sql, ['DEFAULT',
             user.userUUID,
             user.userFirstName,
@@ -42,37 +42,28 @@ class AuthService {
             user.userEmail,
             user.userPassword, user.userRoleId]);
 
-        delete user.userPassword;
         delete user.userId;
+        delete user.userPassword;
 
         const token = cyber.getNewToken(user);
-
         return token;
     }
-    public async login(credentials: CredentialsModel): Promise<string> {
-        // Validate:
-        // User.validate from model:
 
-        // hash password
+    public async login(credentials: CredentialsModel): Promise<string> {
+
         credentials.password = cyber.hashPassword(credentials.password);
 
-        // Create sql:
         const sql = `SELECT * FROM users WHERE userEmail = ? AND userPassword = ?`;
-
-
         const users = await dal.execute(sql, [credentials.email, credentials.password]);
 
-        // getSingleUser
         const user = users[0];
 
         if (!user) throw new Unauthorized("Incorrect email/password");
 
-        delete user.userPassword;
         delete user.userId;
+        delete user.userPassword;
 
-        // Create token for user:
         const token = cyber.getNewToken(user);
-
         return token;
     }
 }
