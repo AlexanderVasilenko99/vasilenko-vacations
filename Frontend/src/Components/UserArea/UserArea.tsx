ac27c552c631bd5a62ebc1f4be137d856de793f2
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-01-23 13:33:20 +0200

    added user page

--- Frontend/src/Components/UserArea/UserArea.tsx
+++ Frontend/src/Components/UserArea/UserArea.tsx
@@ -1,22 +1,80 @@
-import { useParams } from "react-router-dom";
-import "./UserArea.css";
-import { useEffect } from "react";
-import UseTitle from "../../Utils/UseTitle";
+import { useEffect, useState } from "react";
+import UserModel from "../../Models/UserModel";
+import { authStore } from "../../Redux/AuthState";
 import Header from "../Common/header/header";
+import "./UserArea.css";
+import profilePic from "../../Assets/Images/UtilityImages/me_square.jpeg"
+import { useForm } from "react-hook-form";
 
 function UserArea(): JSX.Element {
-    const params = useParams();
-    const uuid = params.uuid;
-    UseTitle(`Vasilenko Vacations | ${uuid}`);
+    const [user, setUser] = useState<UserModel>();
+    const [isFormEditDisabled, setIsFormEditDisabled] = useState<boolean>(true);
+    const { register, handleSubmit, setValue } = useForm<UserModel>();
 
     useEffect(() => {
-        // ADD FRONT AND BACK SERVICES TO GET SINGLE USER FROM HIS UUID
+        const user: UserModel = authStore.getState().user;
+        if (!user) throw new Error("Something went wrong, Please try agin later!");
+        setUser(user);
+        setValue("userFirstName", user.userFirstName);
+        setValue("userLastName", user.userLastName);
+        setValue("userEmail", user.userEmail);
     }, []);
+
+    async function send(user: UserModel) {
+        console.log(user);
+
+    }
 
     return (
         <div className="UserArea">
-            <Header {...{ title: `Hi ${params.uuid}!` }} />
-        </div>
+            <Header {...{ title: `Hi ${user?.userFirstName}!` }} />
+            <div>
+                <div className="edit-button-container">
+                    <button
+                        className="toggle-edit-button"
+                        onClick={() => setIsFormEditDisabled(!isFormEditDisabled)}>
+                        {isFormEditDisabled ? "Enable" : "Disable"} editing
+                    </button>
+                </div>
+                <div className="grid-container">
+
+                    <div className="left">
+                        <img src={profilePic} />
+                    </div>
+
+                    <div className="right">
+                        <form onSubmit={handleSubmit(send)}>
+                            <input
+                                required
+                                type="text"
+                                minLength={2}
+                                maxLength={30}
+                                disabled={isFormEditDisabled}
+                                {...register("userFirstName")}
+                            />
+                            <input
+                                required
+                                type="text"
+                                minLength={2}
+                                maxLength={30}
+                                {...register("userLastName")}
+                                disabled={isFormEditDisabled}
+                            />
+                            <input
+                                required
+                                type="email"
+                                maxLength={50}
+                                {...register("userEmail")}
+                                disabled={isFormEditDisabled}
+                            />
+                            <div>
+                                <button disabled={isFormEditDisabled}>Update</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 }
 
