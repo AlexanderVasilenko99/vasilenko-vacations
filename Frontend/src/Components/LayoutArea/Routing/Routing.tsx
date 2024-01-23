ace465cbdebcd20ec8f0fc23ed7257148e30870b
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-01-23 11:56:09 +0200

    removed a bunch of unnesseccary code

--- Frontend/src/Components/LayoutArea/Routing/Routing.tsx
+++ Frontend/src/Components/LayoutArea/Routing/Routing.tsx
@@ -6,9 +6,6 @@ import Login from "../../AuthArea/Login/Login";
 import RegisterComponent from "../../AuthArea/Register/Register";
 import Home from "../../HomeArea/Home/Home";
 
-import EditProduct from "../../ProductsArea/EditProduct/EditProduct";
-import ProductDetails from "../../ProductsArea/ProductDetails/ProductDetails";
-import ProductsArea from "../../ProductsArea/ProductsArea";
 import UserArea from "../../UserArea/UserArea";
 import VacationPage from "../../VacationsArea/VacationPage/VacationPage";
 import VacationsList from "../../VacationsArea/VacationsList/VacationsList";
@@ -25,10 +22,6 @@ function Routing(): JSX.Element {
                 {/* Default Route */}
                 <Route path="/" element={<Navigate to={"/home"} />}></Route>
                 <Route path="/home/" element={<Home />} />
-                <Route path={appConfig.productsRoute} element={<ProductsArea />} />
-                <Route path="/products/product/:productId/" element={<ProductDetails />} />
-                {/* <Route path="/products/new/" element={<AddProduct />} /> */}
-                <Route path="/products/edit/:prodId" element={<EditProduct />} />
 
                 <Route path="/vacations/" element={<VacationsList />} />
                 <Route path="/vacations/new" element={<AddVacation />} />
@@ -39,7 +32,7 @@ function Routing(): JSX.Element {
                 <Route path="/reports/" element={<ReportsArea />} />
 
                 <Route path="/users/:uuid" element={<UserArea />} />
-                
+
                 <Route path="/register/" element={<RegisterComponent />} />
                 <Route path="/login/" element={<Login />} />
 
--- Frontend/src/Utils/AppConfig.ts
+++ Frontend/src/Utils/AppConfig.ts
@@ -12,19 +12,12 @@ class AppConfig {
     // public readonly editVacationRoute: string = this.vacationsRoute + "edit/"
     // public readonly editVacationDetails: string = this.vacationsRoute + "details/"
 
-    public readonly productsRoute: string = "/products/"
-    public readonly addProductRoute: string = this.productsRoute + "new/"
-    public readonly editProductRoute: string = this.productsRoute + "edit/"
-    public readonly editProductDetails: string = this.productsRoute + "details/"
-
-    public readonly productsUrl: string = 'http://localhost:4000/api/products/';
-
     public readonly vacationsUrl: string = 'http://localhost:4000/api/vacations/';
+    public readonly loginUrl: string = 'http://localhost:4000/api/login/';
+    public readonly registerUrl: string = 'http://localhost:4000/api/register/';
     public readonly vacationsFollowUrl: string = this.vacationsUrl + 'follow/';
     public readonly vacationsUnfollowUrl: string = this.vacationsUrl + 'unfollow/';
     public readonly vacationsImageUrl: string = 'http://localhost:4000/api/vacations-image/';
-    public readonly registerUrl: string = 'http://localhost:4000/api/register/';
-    public readonly loginUrl: string = 'http://localhost:4000/api/login/';
 
     private readonly SITE_PRIMARY_COLOR: string = '#1a5785'
     private readonly SITE_SECONDARY_COLOR: string = '#2e6ba0'
