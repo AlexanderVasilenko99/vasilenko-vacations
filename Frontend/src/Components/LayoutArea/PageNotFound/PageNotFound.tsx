d55433b735e671cfd992f499570c1228600ba4e3
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-02-04 16:23:31 +0200

    corrected indentation on frontend LayoutArea & HomeArea

--- Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
+++ Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
@@ -6,13 +6,11 @@ function PageNotFound(): JSX.Element {
     UseTitle("Vasilenko Vacations | 404");
     return (
         <div className="PageNotFound">
-
             <img src={backgroundImage} alt="" />
             <div>
                 <p>Looks like you are looking for<br /> a page that doesn't exists!</p>
                 <p>Maybe you would like to try our <NavLink to="/vacations">vacations</NavLink> page instead?</p>
             </div>
-
         </div >
     );
 }
--- Frontend/src/Components/LayoutArea/Routing/Routing.tsx
+++ Frontend/src/Components/LayoutArea/Routing/Routing.tsx
@@ -1,18 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-
-import appConfig from "../../../Utils/AppConfig";
 import AboutArea from "../../AboutArea/AboutArea";
 import Login from "../../AuthArea/Login/Login";
 import RegisterComponent from "../../AuthArea/Register/Register";
 import Home from "../../HomeArea/Home/Home";
-
+import ReportsArea from "../../ReportsArea/ReportsArea";
 import UserArea from "../../UserArea/UserArea";
+import AddVacation from "../../VacationsArea/AddVacation/AddVacation";
 import VacationPage from "../../VacationsArea/VacationPage/VacationPage";
 import VacationsList from "../../VacationsArea/VacationsList/VacationsList";
 import PageNotFound from "../PageNotFound/PageNotFound";
-import AddVacation from "../../VacationsArea/AddVacation/AddVacation";
-import ReportsArea from "../../ReportsArea/ReportsArea";
-import "./Routing.css"
+import "./Routing.css";
 
 
 function Routing(): JSX.Element {
@@ -22,23 +19,15 @@ function Routing(): JSX.Element {
                 {/* Default Route */}
                 <Route path="/" element={<Navigate to={"/home"} />}></Route>
                 <Route path="/home/" element={<Home />} />
-
                 <Route path="/vacations/" element={<VacationsList />} />
                 <Route path="/vacations/new" element={<AddVacation />} />
                 <Route path="/vacations/:uuid" element={<VacationPage />} />
-                {/* <Route path="/vacations/:uuid" element={<EditVacation />} /> */}
-
-                <Route path="/about/" element={<AboutArea />} />
-                <Route path="/reports/" element={<ReportsArea />} />
-
-                <Route path="/users/:uuid" element={<UserArea />} />
-
                 <Route path="/register/" element={<RegisterComponent />} />
+                <Route path="/reports/" element={<ReportsArea />} />
+                <Route path="/users/:uuid" element={<UserArea />} />
+                <Route path="/about/" element={<AboutArea />} />
                 <Route path="/login/" element={<Login />} />
-
-
                 <Route path="/*" element={<PageNotFound />}></Route>
-
             </Routes>
         </div>
     );
