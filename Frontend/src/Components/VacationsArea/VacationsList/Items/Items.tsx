567b87835b71b2ba2d678a3ea4d8f3214fdac2ec
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-02-14 13:50:16 +0200

    added no matching results

--- Frontend/src/Components/VacationsArea/VacationsList/Items/Items.tsx
+++ Frontend/src/Components/VacationsArea/VacationsList/Items/Items.tsx
@@ -21,6 +21,7 @@ function Items({ currentItems }: ItemsPropsModel): JSX.Element {
                     vacationIsFollowing={v.vacationIsFollowing}
                     vacationFollowersCount={v.vacationFollowersCount} />
                 )}
+            {currentItems.length === 0 && <span>No matching results</span>}
         </>
     );
 }
